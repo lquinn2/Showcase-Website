eb13ed742e5bd40e385fa9c8faa6d3389a57c985
Liam <liambarryquinn@gmail.com>
2019-03-29 01:45:22 -0700

    Changed navigation menu animation from slide to fade, Also added proper links to footer link tags

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,24 +15,32 @@ $('#img-logo-top').css('opacity', '1');
 $('#img-logo-bottom').css('opacity', '0');
 $('#img-logo-middle').css('opacity', '0');
 
-// Side-nav movement animator
+// Initial menu fade in on page load
+$('.side-nav-top').css('opacity', '1');
+$('.side-nav-middle').css('opacity', '0');
+$('.side-nav-bottom').css('opacity', '0');
+
+// Navigation menu and logo animator
 $(window).scroll(function() {
   if (!resetPointActivated) {
     // This one is for scrolling back up
     if($(window).scrollTop() < resetPoint) {
-      sideNav.style.top = "480px";
-
-      // Change color to yellow on hover
+      // Change nav-item color to yellow on hover
       $('nav > a').mouseenter(function() {
         $(this).css('color', '#f1c244');
       }).mouseleave(function() {
         $(this).css('color', 'white');
       });
 
-      // Make logo fade in
+      // Make logo fade in and fade others out
       $('#img-logo-top').css('opacity', '1');
       $('#img-logo-middle').css('opacity', '0');
       $('#img-logo-bottom').css('opacity', '0');
+
+      // Make navigation menu fade in and others fade out
+      $('.side-nav-top').css('opacity', '1');
+      $('.side-nav-middle').css('opacity', '0');
+      $('.side-nav-bottom').css('opacity', '0');
 
       firstActivatePointActivated = false;
       secondActivatePointActivated = false;
@@ -41,13 +49,7 @@ $(window).scroll(function() {
   if (firstActivatePointActivated) {    
     // This one is for scrolling back up
     if(($(window).scrollTop() + $(window).height()) <= 2400) {
-      sideNav.style.top = "480px";
-
-      for (var i = 0; i < sideNav.childElementCount; i++) {
-        sideNav.children[i].style.color = "white";
-      }
-
-      // Change color to yellow on hover
+      // Change nav-item color to yellow on hover
       $('nav > a').mouseenter(function() {
         $(this).css('color', '#f1c244');
       }).mouseleave(function() {
@@ -59,24 +61,23 @@ $(window).scroll(function() {
       $('#img-logo-middle').css('opacity', '1');
       $('#img-logo-bottom').css('opacity', '0');
 
+      // Make navigation menu fade in and others fade out
+      $('.side-nav-top').css('opacity', '0');
+      $('.side-nav-middle').css('opacity', '1');
+      $('.side-nav-bottom').css('opacity', '0');
+
       firstActivatePointActivated = false;
       secondActivatePointActivated = false;
     }
   }
   if (!firstActivatePointActivated) {
     if($(window).scrollTop() > firstActivatePoint) {
-      sideNav.style.top = "1250px";
       firstActivatePointActivated = true;
     } 
   }
   if (!secondActivatePointActivated) {
     if ($(window).scrollTop() > secondActivatePoint) {
-      sideNav.style.top = "2315px";
-      for (var x = 0; x < sideNav.childElementCount; x++) {
-        sideNav.children[x].style.color = "#19414c";
-      }
-
-      // Change color to yellow on hover
+      // Change nav-item color to yellow on hover
       $('nav > a').mouseenter(function() {
         $(this).css('color', '#f1c244');
       }).mouseleave(function() {
@@ -87,6 +88,11 @@ $(window).scroll(function() {
       $('#img-logo-top').css('opacity', '0');
       $('#img-logo-middle').css('opacity', '0');
       $('#img-logo-bottom').css('opacity', '1');
+
+      // Make navigation menu fade in and others fade out
+      $('.side-nav-top').css('opacity', '0');
+      $('.side-nav-middle').css('opacity', '0');
+      $('.side-nav-bottom').css('opacity', '1');
 
       secondActivatePointActivated = true;
     }
